ca0b6abd41ef2b2210a5fb6c9a954a19cec9a39c
iMaxXie <xyang.xd@gmail.com>
2013-11-01 10:37:45 +0800

    目录结构 示意

---
+++ src/main/java/com/pro/pocketnewscms/control/activity/XXAction.java
@@ -0,0 +1,11 @@
+package com.pro.pocketnewscms.control.activity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-11-1
+ * Time: 上午10:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class XXAction {
+}
